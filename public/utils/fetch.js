0cf43409f626bd49bda4a3618b88b1fe6c61afa3
Joseph Tatum <josephtatum@gmail.com>
2019-12-13 13:52:50 -0800

    calling cors

--- public/utils/fetch.js
+++ public/utils/fetch.js
@@ -1,8 +1,8 @@
-const URL = '';
-https://secure-brook-84158.herokuapp.com
+const URL = 'https://secure-brook-84158.herokuapp.com';
+
 const getRecipes = async() => {
-  const response = await fetch(`${URL}/api/v1/recipes`);
-  console.log(response);
+  const recipes = await fetch(`${URL}/api/v1/recipes`);
+  console.log(recipes);
 };
 
 getRecipes();
