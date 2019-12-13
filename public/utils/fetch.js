bb98ebfcd7ef8f8cef2e849a06467726893b1332
Joseph Tatum <josephtatum@gmail.com>
2019-12-13 12:52:15 -0800

    working?

--- public/utils/fetch.js
+++ public/utils/fetch.js
@@ -1,5 +1,5 @@
-const URL = 'https://secure-brook-84158.herokuapp.comnpm';
-
+const URL = '';
+https://secure-brook-84158.herokuapp.com
 const getRecipes = async() => {
   const response = await fetch(`${URL}/api/v1/recipes`);
   console.log(response);
