7d415f4e388807a42d53d1c8ea9643da1f307185
Joseph Tatum <josephtatum@gmail.com>
2019-12-15 18:20:59 -0800

    made some progress on front end side

--- public/utils/fetch.js
+++ public/utils/fetch.js
@@ -1,7 +1,7 @@
 const URL = 'https://secure-brook-84158.herokuapp.com';
 
 const getRecipes = async() => {
-  const recipes = await fetch(`${URL}/api/v1/recipes`);
+  const recipes = await fetch(`${URL}`);
   console.log(recipes);
 };
 
